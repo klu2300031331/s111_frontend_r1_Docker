55d9bfb85fc68fae403018ccc5fe44c18e171519
Thota Gowtham <163727915+klu2300031331@users.noreply.github.com>
2025-09-24 09:05:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // Make sure it matches package.json
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce/'   // ✅ THIS IS IMPORTANT
-})
+  base: '/ecommerce/', // ✅ This must match your Tomcat context
+});
